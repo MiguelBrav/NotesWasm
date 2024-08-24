46b5588a903d7a83727e1e0467844d236cf0f1f2
MiguelBrav <miguel_98-@hotmail.com>
2024-08-23 22:41:38 -0600

    notes project with local storage

---
+++ Data/Language.cs
@@ -0,0 +1,38 @@
+﻿namespace NotesWasm.Data;
+
+public class Language
+{
+
+    private static readonly Dictionary<string, Dictionary<string, string>> Translations = new Dictionary<string, Dictionary<string, string>>
+    {
+        { "HomeTitle", new Dictionary<string, string> { { "en", "Home" }, { "es", "Inicio" } } },
+        { "NotesTitle", new Dictionary<string, string> { { "en", "Notes" }, { "es", "Notas" } } },
+        { "Instruction", new Dictionary<string, string> { { "en", "Add your local user name" }, { "es", "Agrega tu nombre de usuario local" } } },
+        { "Greetings", new Dictionary<string, string> { { "en", "Welcome!" }, { "es", "Bienvenido!" } } },
+        { "MessageWelcome", new Dictionary<string, string> {
+        { "en", "In this web application, you can save personal notes quickly and easily. You can add, edit, and delete your notes at any time, keeping all your information organized and accessible. Start managing your ideas and reminders today!" }, 
+        { "es", "En esta aplicación web, tienes la capacidad de guardar notas personales de manera rápida y sencilla. Puedes añadir, editar y eliminar tus notas en cualquier momento, manteniendo toda tu información organizada y accesible. ¡Empieza a gestionar tus ideas y recordatorios hoy mismo!" } } },
+        { "Add", new Dictionary<string, string> { { "en", "Add" }, { "es", "Agregar" } } },
+        { "Edit", new Dictionary<string, string> { { "en", "Edit" }, { "es", "Editar" } } },
+        { "Delete", new Dictionary<string, string> { { "en", "Delete" }, { "es", "Eliminar" } } },
+        { "Save", new Dictionary<string, string> { { "en", "Save" }, { "es", "Guardar" } } },
+        { "User", new Dictionary<string, string> { { "en", "User" }, { "es", "Usuario" } } },
+        { "Title", new Dictionary<string, string> { { "en", "Title" }, { "es", "Título" } } },
+        { "Content", new Dictionary<string, string> { { "en", "Description" }, { "es", "Descripción" } } },
+        { "Actions", new Dictionary<string, string> { { "en", "Actions" }, { "es", "Acciones" } } },
+        { "Return", new Dictionary<string, string> { { "en", "Return" }, { "es", "Volver" } } },
+        { "Page", new Dictionary<string, string> { { "en", "Page" }, { "es", "Página" } } }
+    };
+
+    public static string GetTranslation(string key, string languageCode)
+    {
+        if (Translations.TryGetValue(key, out var translations) &&
+            translations.TryGetValue(languageCode, out var translation))
+        {
+            return translation;
+        }
+        return key;
+    }
+
+}
+
---
+++ Models/Note.cs
@@ -0,0 +1,8 @@
+﻿namespace NotesWasm.Models;
+
+public class Note
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;    
+}
